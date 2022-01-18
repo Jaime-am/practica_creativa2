c055ac630bdfdaae174fce5422f3e6dce004c31a
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-01-18 20:32:08 +0100

    Update p_c2_pp.py

--- p_c2_pp.py
+++ p_c2_pp.py
@@ -3,4 +3,3 @@ import os
 os.system("git clone https://github.com/CDPS-ETSIT/practica_creativa2.git")
 os.system("pip3 install -r bookinfo/src/productpage/requirements.txt")
 os.system("pip3 install flask")
-os.system("python3 productpage.py")
