658516ec03a0f21a7459aa6dc3f554d46e841e71
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-01-19 18:35:05 +0100

    Update p_c2_pp.py

--- p_c2_pp.py
+++ p_c2_pp.py
@@ -3,5 +3,5 @@ import os
 os.system("git clone https://github.com/CDPS-ETSIT/practica_creativa2.git")
 os.system("pip3 install -r practica_creativa2/bookinfo/src/productpage/requirements.txt")
 os.system("pip install -r practica_creativa2/bookinfo/src/productpage/requirements.txt")
-os.system("pip install Bootstrap")
-os.system("pip3 install Bootstrap")
+os.system("pip install flask_bootstrap")
+os.system("pip3 install flask_bootstrap")
