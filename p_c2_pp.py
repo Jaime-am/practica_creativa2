ac5e4f541eca058db6de8b6d1735c3d402feecfb
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-01-19 18:38:24 +0100

    Update p_c2_pp.py

--- p_c2_pp.py
+++ p_c2_pp.py
@@ -2,6 +2,5 @@ import os
 
 os.system("git clone https://github.com/CDPS-ETSIT/practica_creativa2.git")
 os.system("pip3 install -r practica_creativa2/bookinfo/src/productpage/requirements.txt")
-os.system("pip install -r practica_creativa2/bookinfo/src/productpage/requirements.txt")
-os.system("pip install flask_bootstrap")
 os.system("pip3 install flask_bootstrap")
+os.system("pip3 install jaeger_client")
