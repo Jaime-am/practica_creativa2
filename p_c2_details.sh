9bed53eb158948b2156f968c43ac5ffacc245346
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-01-20 19:35:36 +0100

    Update p_c2_details.sh

--- p_c2_details.sh
+++ p_c2_details.sh
@@ -1,2 +1,2 @@
 git clone https://github.com/CDPS-ETSIT/practica_creativa2.git
-cp practica_creativa2/bookinfo/src/details/details.rb /opt/microservices
+cp practica_creativa2/bookinfo/src/details/details.rb /opt/microservices/details.rb
