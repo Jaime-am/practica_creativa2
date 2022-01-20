d6d4aa4c6a21a5d57ec49c54b7f17ba12c63d079
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-01-20 21:57:32 +0100

    Update p_c2_ratings.sh

--- p_c2_ratings.sh
+++ p_c2_ratings.sh
@@ -2,4 +2,4 @@ git clone https://github.com/CDPS-ETSIT/practica_creativa2.git
 mkdir /opt/microservices
 cp practica_creativa2/bookinfo/src/ratings/package.json /opt/microservices/package.json
 cp practica_creativa2/bookinfo/src/ratings/ratings.js /opt/microservices/ratings.js
-npm install /opt/microservices
+node install /opt/microservices
