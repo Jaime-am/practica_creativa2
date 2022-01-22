95b693389cb618e510c562cc9110e72fc23dfc03
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2022-01-22 11:47:12 +0100

    Update p_c2_pp.py

--- p_c2_pp.py
+++ p_c2_pp.py
@@ -1,5 +1,6 @@
 import os
 
+os.system("/usr/local/bin/python -m pip install --upgrade pip")
 os.system("git clone https://github.com/CDPS-ETSIT/practica_creativa2.git")
 os.system("pip3 install -r practica_creativa2/bookinfo/src/productpage/requirements.txt")
 fin = open("./practica_creativa2/bookinfo/src/productpage/requirements.txt", "r")
